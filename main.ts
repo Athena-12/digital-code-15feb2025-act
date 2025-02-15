c99652879c305f801de8bbae9e6f56271f4087ad
Athena-12 <cabugon.eloisa@gmail.com>
2025-02-15 11:08:36 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
 let NUMBERS = 0
 input.onGesture(Gesture.Shake, function () {
-    NUMBERS = randint(1, 6)
+    NUMBERS = randint(1, 3)
+    if (NUMBERS == 1) {
+        basic.showIcon(IconNames.Scissors)
+        music.play(music.tonePlayable(330, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    } else if (NUMBERS == 2) {
+        basic.showIcon(IconNames.SmallSquare)
+        music.play(music.tonePlayable(233, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    } else if (NUMBERS == 3) {
+        basic.showIcon(IconNames.Square)
+        music.play(music.tonePlayable(165, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    }
 })
